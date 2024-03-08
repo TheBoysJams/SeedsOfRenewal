4b56257979477d5011357f8bf1908c17cafd18c2
Michael Wheatley <miatribe@users.noreply.github.com>
2024-03-08 16:52:56 -0500

    Bit of folder cleanup
    cleanup

--- RenewalTreeDefence/Main Menu/MainMenu.gd
+++ RenewalTreeDefence/Main Menu/MainMenu.gd
@@ -1,18 +1,14 @@
 extends MarginContainer
 
+@export var levelToLoad: PackedScene
 
-func _ready():
-	$VBoxContainer/StartButton.connect("pressed", Callable(self, "_on_StartButton_pressed"))
-	$VBoxContainer/ExitButton.connect("pressed", Callable(self, "_on_ExitButton_pressed"))
-
-func _on_StartButton_pressed():
-	# Load the level scene as a PackedScene object
-	var scene = load("res://prototype_level.tscn") as PackedScene
-	# Ensure the scene is not null
-	if scene:
+func _on_start_button_pressed() -> void:
+		# Load the level scene as a PackedScene object
+	# Ensure the levelToLoad is not null
+	if levelToLoad:
 		# Change to the loaded scene
-		get_tree().change_scene_to_packed(scene)
+		get_tree().change_scene_to_packed(levelToLoad)
 
-func _on_ExitButton_pressed():
-	# Quit the game
+func _on_exit_button_pressed() -> void:
+		# Quit the game
 	get_tree().quit()
