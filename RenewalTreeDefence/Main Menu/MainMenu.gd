5ef95f4c033596b401402a531d5415a64f635e78
neswartz <46683852+neswartz@users.noreply.github.com>
2024-03-07 20:16:39 -0800

    Merge main_menu changes

--- RenewalTreeDefence/Main Menu/MainMenu.gd
+++ RenewalTreeDefence/Main Menu/MainMenu.gd
@@ -1,9 +1,9 @@
 extends MarginContainer
 
+
 func _ready():
-	$StartButton.connect("pressed", Callable(self, "_on_StartButton_pressed"))
-	$ExitButton.connect("pressed", Callable(self, "_on_ExitButton_pressed"))
-	$VolumeSlider.connect("value_changed", Callable(self, "_on_VolumeSlider_value_changed"))
+	$VBoxContainer/StartButton.connect("pressed", Callable(self, "_on_StartButton_pressed"))
+	$VBoxContainer/ExitButton.connect("pressed", Callable(self, "_on_ExitButton_pressed"))
 
 func _on_StartButton_pressed():
 	# Load the level scene as a PackedScene object
@@ -16,8 +16,3 @@ func _on_StartButton_pressed():
 func _on_ExitButton_pressed():
 	# Quit the game
 	get_tree().quit()
-
-func _on_VolumeSlider_value_changed(value):
-	# Adjust the volume. This is a placeholder for wherever you manage your game's audio.
-	# You might need to adjust this based on how you've implemented audio in your game.
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
--- RenewalTreeDefence/Main Menu/VolumeSlider.gd
+++ RenewalTreeDefence/Main Menu/VolumeSlider.gd
@@ -5,7 +5,7 @@ var audio_bus_name = "Music"
 @onready var _bus = AudioServer.get_bus_index(audio_bus_name)
 
 func _ready():
-	AudioServer.set_bus_volume_db(_bus, linear_to_db(0.5))
+	AudioServer.set_bus_volume_db(_bus, linear_to_db(10))
 	value = db_to_linear(AudioServer.get_bus_volume_db(_bus))
 	
 func _on_value_changed(value):
