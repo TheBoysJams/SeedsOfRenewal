d7fc93224dec17d795bb2f4bd733837c7688c7a5
neswartz <46683852+neswartz@users.noreply.github.com>
2024-03-08 14:49:54 -0800

    Add pause menu

---
+++ RenewalTreeDefence/UI/PauseMenu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Connect the buttons to their signals
+	$MarginContainer/VBoxContainer/ResumeButton.connect("pressed", Callable(self, "_on_ResumeButton_pressed"))
+	$MarginContainer/VBoxContainer/ExitToMenuButton.connect("pressed", Callable(self, "_on_ExitButton_pressed"))
+	# Initially hide the pause menu
+	visible = false
+
+func _on_ResumeButton_pressed():
+	# Resume the game
+	get_tree().paused = false
+	visible = false
+
+
+func _on_ExitButton_pressed():
+	# Exit to the main menu
+	var main_menu_scene = load("res://UI/main_menu.tscn") as PackedScene
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(main_menu_scene)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
